9c14ee338d11836b4358efc46088b82d3f3b2f2f
PandaaRojo <manuelffselva@gmail.com>
2024-05-31 18:56:09 -0300

    Update camaraFront.js

--- camaraFront.js
+++ camaraFront.js
@@ -1,30 +1,25 @@
-// Obtener la lista de dispositivos multimedia
 navigator.mediaDevices.enumerateDevices().then(devices => {
-    devices = devices.filter(device => device.kind === 'videoinput');
-    
-    // Encontrar la cámara frontal
-    let frontCamera = devices.find(device => device.label.toLowerCase().includes('front'));
-    
-    if (!frontCamera) {
-      alert('No se encontró la cámara frontal');
-      return;
+  devices = devices.filter(device => device.kind === 'videoinput');
+  
+  let frontCamera = devices.find(device => device.label.toLowerCase().includes('front'));
+  
+  if (!frontCamera) {
+    alert('No se encontró la cámara frontal');
+    return;
+  }
+  
+  let constraints = {
+    video: {
+      deviceId: { exact: frontCamera.deviceId }
     }
-    
-    // Configurar las opciones de la cámara
-    let constraints = {
-      video: {
-        deviceId: { exact: frontCamera.deviceId }
-      }
-    };
-    
-    // Obtener el acceso a la cámara
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(stream => {
-        // Reemplazar el video de AR.js con el stream de la cámara frontal
-        let video = document.querySelector('video');
-        video.srcObject = stream;
-      })
-      .catch(error => {
-        console.error('Error al obtener la cámara frontal:', error);
-      });
-  });
+  };
+  
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+      let video = document.querySelector('video');
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      console.error('Error al obtener la cámara frontal:', error);
+    });
+});
